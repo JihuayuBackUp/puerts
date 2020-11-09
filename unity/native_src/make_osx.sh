2b020c8edf699af2df608647b065b08b49ffac4a
jihuayu <8042833@qq.com>
2020-11-09 21:30:38 +0800

    Delete All

--- unity/native_src/make_osx.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build_osx && cd build_osx
-cmake -GXcode ../
-cd ..
-cmake --build build_osx --config Release
-mkdir -p ../Assets/Plugins/
-cp -r build_osx/Release/puerts.bundle ../Assets/Plugins/
-
